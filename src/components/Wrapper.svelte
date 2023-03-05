e87ef4580161c105b1ea29cea324c62e5164dec4
Michal <hi@geodev.me>
2023-03-05 19:38:12 +0100

    Create base project structure

---
+++ src/components/Wrapper.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Button from "./Button.svelte";
+</script>
+
+<div class="flex flex-col bg-neutral-300 h-auto w-96 rounded-3xl p-5">
+    <Button text="Generate" />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Wrapper from "../components/Wrapper.svelte";
+</script>
+
+<div class="flex flex-col items-center min-h-screen p-2 bg-amber-100">
+    <h1 class="text-lg font-bold text-stone-700 m-5">Mosaic Generator</h1>
+    <Wrapper />
+</div>
+

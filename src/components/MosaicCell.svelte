e30844a4dbc01197e1bae935a0b40cd833ea8af9
Michal <hi@geodev.me>
2023-03-05 20:32:12 +0100

    Generate grid with given rows and columns

---
+++ src/components/MosaicCell.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let content;
+</script>
+
+<div class="bg-emerald-400 rounded w-20 h-20">{content}</div>
--- src/components/Wrapper.svelte
+++ src/components/Wrapper.svelte
@@ -1,7 +1,31 @@
-<script>
+<script lang="ts">
     import Button from "./Button.svelte";
+    import GridInput from "./GridInput.svelte";
+    import MosaicCell from "./MosaicCell.svelte";
+
+    let gridColumns = 0;
+    let gridRows = 0;
+    let cells;
+
+    function handleGridGenerate() {
+      cells = gridColumns * gridRows;
+    }
+
 </script>
 
-<div class="flex flex-col bg-neutral-300 h-auto w-96 rounded-3xl p-5">
+<div class="flex flex-col bg-neutral-300 h-auto min-w-96 rounded-3xl p-5">
+    <div class="flex">
+        <GridInput inputName="rows" inputText="Rows" bind:inputValue={gridRows} />
+        <GridInput inputName="columns" inputText="Columns" bind:inputValue={gridColumns} />
+    </div>
+    <div class={`grid grid-cols-${gridColumns} grid-rows-${gridRows} gap-4 my-5`}>
+        {#if cells}
+            {#each Array(cells) as _, cell}
+                <MosaicCell content={cell}/>
+            {/each}
+        {/if}
+    </div>
+
+    <Button text="Create grid" onClick={handleGridGenerate} />
     <Button text="Generate" />
 </div>

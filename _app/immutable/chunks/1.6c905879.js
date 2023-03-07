b382a5089858a728b3f7120cb8e706ba1f52062f
Michal <hi@geodev.me>
2023-03-07 20:45:52 +0100

    Updates

---
+++ _app/immutable/chunks/1.6c905879.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.c98735c7.js";export{t as component};
--- _app/immutable/chunks/1.968c0bdb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.ce7a5f94.js";export{t as component};

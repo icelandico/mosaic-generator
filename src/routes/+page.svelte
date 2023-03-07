ed8ce66a3530b7258ad8b2a50195eba1e6e79f32
Michal <hi@geodev.me>
2023-03-07 20:59:04 +0100

    Set correct config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col items-center min-h-screen p-2 bg-amber-100">
-    <h1 class="text-lg font-bold text-stone-700 m-5">Mosaic Generator Ver. 2</h1>
+    <h1 class="text-lg font-bold text-stone-700 m-5">Mosaic Generator Ver. 3</h1>
     <Wrapper />
 </div>
 
